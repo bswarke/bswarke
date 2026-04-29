37e8c0f68c5c759b7c9f14ab286b95cfa7fa5504
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:13:14 +0500

    Update 09.py

--- Пр15/09.py
+++ Пр15/09.py
@@ -5,4 +5,4 @@ with open('result.txt', 'w', encoding='utf-8') as outfile:
         if os.path.exists(filename):
             with open(filename, 'r', encoding='utf-8') as infile:
                 outfile.write(infile.read())
-print("Файлы объединены в result.txt")
+print("The file is merged into result.txt")
