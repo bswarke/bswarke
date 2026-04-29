39ae0921edd0512510c5f552a5ff646f8af5c9ff
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:30:21 +0500

    Update 20.py

--- Пр17/20.py
+++ Пр17/20.py
@@ -4,17 +4,13 @@ import os
 
 filename = "numbers.txt"
 
-# генерация чисел
 numbers = [random.randint(1, 100) for _ in range(5)]
 
-# запись
 with open(filename, "w") as f:
-    f.write(f"Дата: {datetime.now()}\n")
-    f.write("Числа: " + " ".join(map(str, numbers)))
+    f.write(f"Date: {datetime.now()}\n")
+    f.write("Number: " + " ".join(map(str, numbers)))
 
-# проверка файла
-print("Файл существует:", os.path.exists(filename))
+print("File exists:", os.path.exists(filename))
 
-# чтение
 with open(filename, "r") as f:
     print(f.read())
