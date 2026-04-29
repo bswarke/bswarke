5feef8f8fbb13d25c1b08d128b29c84f9005a55b
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:14:28 +0500

    Update 11.py

--- Пр15/11.py
+++ Пр15/11.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 date_obj = datetime.strptime("2024-12-31", "%Y-%m-%d")
-print(f"День: {date_obj.day}, Месяц: {date_obj.month}, Год: {date_obj.year}")
+print(f"Day: {date_obj.day}, Month: {date_obj.month}, Year: {date_obj.year}")
