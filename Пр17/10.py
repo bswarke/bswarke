b49dc7411efc99b6e8259c24f0d3ed2f5ed509f0
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:26:58 +0500

    Update 10.py

--- Пр17/10.py
+++ Пр17/10.py
@@ -3,5 +3,5 @@ import datetime
 current_date = datetime.date.today()
 future_date = current_date + datetime.timedelta(days=7)
 
-print(f"Текущая дата: {current_date}")
-print(f"Дата через 7 дней: {future_date}")
+print(f"Current date: {current_date}")
+print(f"Date in 7 days: {future_date}")
