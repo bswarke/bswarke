a21737be38520d1bdbe89ed2936a3e2e1e32edf0
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:21:43 +0500

    Update 19.py

--- Пр16/19.py
+++ Пр16/19.py
@@ -4,4 +4,4 @@ text19 = "Name: John Age: 25"
 name_match = re.search(r'Name:\s+(\w+)', text19)
 age_match = re.search(r'Age:\s+(\d+)', text19)
 if name_match and age_match:
-    print(f"Имя: {name_match.group(1)}, Возраст: {age_match.group(1)}")
+    print(f"Name: {name_match.group(1)}, Age: {age_match.group(1)}")
