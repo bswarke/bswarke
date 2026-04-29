f2a5cb6028f7ad6fe60711386275eaa9cbf8b75a
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:16:21 +0500

    Update 14.py

--- Пр15/14.py
+++ Пр15/14.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
-birth = datetime.strptime(input("Введите дату рождения (ГГГГ-ММ-ДД): "), "%Y-%m-%d")
+birth = datetime.strptime(input("enter date of birth (YYYY-MM-DD): "), "%Y-%m-%d")
 age_days = (datetime.now() - birth).days
-print(f"Возраст в днях: {age_days}")
+print(f"Age in days: {age_days}")
