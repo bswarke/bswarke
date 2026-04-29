a944f792f3c9cdb427225d7da8304cb77ffd141b
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:29:10 +0500

    Update 16.py

--- Пр17/16.py
+++ Пр17/16.py
@@ -5,5 +5,5 @@ file_name = "data.json"
 with open(file_name, 'r', encoding='utf-8') as f:
     loaded_data = json.load(f)
 
-print("Содержимое файла 'data.json':")
+print("File Contents 'data.json':")
 print(loaded_data)
