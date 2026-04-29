0ba6c92d4a0ba7b3db8bc2813bc29df2db2c0aaf
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:11:01 +0500

    Update 04.py

--- Пр15/04.py
+++ Пр15/04.py
@@ -4,4 +4,4 @@ with open('data.csv', 'r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
     ages = [int(row['age']) for row in reader]
 avg_age = sum(ages) / len(ages)
-print(f"Средний возраст: {avg_age}")
+print(avg_age)
