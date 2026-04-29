55f97e7ee49b42f271fc625ff0eaf82eb7e16e6a
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:10:18 +0500

    Update 01.py

--- Пр15/01.py
+++ Пр15/01.py
@@ -4,4 +4,4 @@ with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 words = re.findall(r'\b\w+\b', text.lower())
 unique_count = len(set(words))
-print(f"Количество уникальных слов: {unique_count}")
+print(unique_count)
