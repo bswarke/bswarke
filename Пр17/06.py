8b9854329e52efd1a23efeafb0eccbce0ce4ca00
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:25:16 +0500

    Update 06.py

--- Пр17/06.py
+++ Пр17/06.py
@@ -2,4 +2,4 @@ import random
 
 my_list_to_shuffle = [1, 2, 3, 4, 5]
 random.shuffle(my_list_to_shuffle)
-print(f"Перемешанный список: {my_list_to_shuffle}")
+print(f"Mixed list: {my_list_to_shuffle}")
