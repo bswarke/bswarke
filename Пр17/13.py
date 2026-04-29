981d7760f1971809eb310b60070bb78ec68823de
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:27:56 +0500

    Update 13.py

--- Пр17/13.py
+++ Пр17/13.py
@@ -4,6 +4,6 @@ file_name = 'data.txt'
 
 if os.path.exists(file_name):
     file_size = os.path.getsize(file_name)
-    print(f"Размер файла '{file_name}': {file_size} байт.")
+    print(f"File size '{file_name}': {file_size} bite.")
 else:
-    print(f"Файл '{file_name}' не найден.")
+    print(f"File '{file_name}' not found.")
