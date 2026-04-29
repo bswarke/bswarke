fff629c96da4cb292082fd9dcda645651696cd82
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:13:45 +0500

    Update 10.py

--- Пр15/10.py
+++ Пр15/10.py
@@ -2,4 +2,4 @@ with open('input.txt', 'r', encoding='utf-8') as f:
     unique_lines = set(f.readlines())
 with open('unique_lines.txt', 'w', encoding='utf-8') as f:
     f.writelines(unique_lines)
-print("”никальные строки записаны в unique_lines.txt")
+print("Unique strings are written in unique_lines.txt")
