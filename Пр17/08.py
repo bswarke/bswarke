0d7c7706a01613d4673eb37cbd05f39605aa9e14
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:26:09 +0500

    Update 08.py

--- Пр17/08.py
+++ Пр17/08.py
@@ -2,4 +2,4 @@ import datetime
 
 current_date = datetime.date.today()
 formatted_date = current_date.strftime("%Y-%m-%d")
-print(f"Текущая дата в формате YYYY-MM-DD: {formatted_date}")
+print(f"Current date in format YYYY-MM-DD: {formatted_date}")
