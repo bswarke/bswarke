9539ca52b279760bcc8bf4e2ba77c9b748951a5e
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:11:19 +0500

    Update 05.py

--- Пр15/05.py
+++ Пр15/05.py
@@ -3,7 +3,7 @@ import csv
 with open('data.csv', 'r', encoding='utf-8') as f:
     lines = f.readlines()
 with open('filtered.csv', 'w', encoding='utf-8') as f:
-    f.write(lines[0]) # заголовок
+    f.write(lines[0]) 
     for line in lines[1:]:
         age = int(line.strip().split(',')[1])
         if age > 25:
