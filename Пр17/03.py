5c264caba65b9f4d4408f926b0ec130f717a87b3
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:23:51 +0500

    Update 03.py

--- Пр17/03.py
+++ Пр17/03.py
@@ -5,6 +5,6 @@ number = 3.7
 rounded_down = math.floor(number)
 rounded_up = math.ceil(number)
 
-print(f"Число: {number}")
-print(f"Округление вниз (math.floor): {rounded_down}")
-print(f"Округление вверх (math.ceil): {rounded_up}")
+print(f"Number: {number}")
+print(f"Round down (math.floor): {rounded_down}")
+print(f"Round up (math.ceil): {rounded_up}")
