c72805e47032580e63e6783c654d6392a8103e85
bswarke <kseniastepanova853@gmail.com>
2026-04-29 13:21:09 +0500

    Update 17.py

--- Пр16/17.py
+++ Пр16/17.py
@@ -3,6 +3,6 @@ import re
 log_line = "2026-04-01 ERROR Something failed"
 match = re.search(r'(\d{4}-\d{2}-\d{2})\s+(INFO|ERROR)\s+(.+)', log_line)
 if match:
-    print(f"Дата: {match.group(1)}")
-    print(f"Уровень: {match.group(2)}")
-    print(f"Сообщение: {match.group(3)}")
+    print(f"Date: {match.group(1)}")
+    print(f"Level: {match.group(2)}")
+    print(f"Message: {match.group(3)}")
